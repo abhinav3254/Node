eeece6585d9ee5c284d0bd3bfdc2df047250e9a5
abhinav3254 <abhinav3254@gmail.com>
2022-05-30 21:01:15 +0530

    Unit 3

---
+++ unit3/index.js
@@ -0,0 +1,14 @@
+console.log("Abhinav");
+
+// To run this code we have to go this directory and there we have to type
+// node filename.js
+
+// Example node index.js
+// and we see the output of the code
+
+var a = 20;
+var b = 30;
+
+console.log(a+b);
+
+console.warn(a+b);
