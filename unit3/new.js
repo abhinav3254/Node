381baba0dfe021eec7eb7c37dc4e0e5aa3d117b0
abhinav3254 <abhinav3254@gmail.com>
2022-06-01 12:25:18 +0530

    Revise

---
+++ unit3/new.js
@@ -0,0 +1,15 @@
+
+
+var a = 10;
+
+var b = 30;
+
+console.log(a+b)
+
+console.warn('This is Abhinav Jha')
+
+console.log('This is Node js tutorial')
+
+console.log('Todays date is 1/6/2022')
+
+console.log("Make API using Node js")
