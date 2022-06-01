0f9fd238a3a57bd1d97affda74a77681f1ad7d6a
abhinav3254 <abhinav3254@gmail.com>
2022-06-01 13:01:40 +0530

    import and export in node

---
+++ unit4/export.js
@@ -0,0 +1,8 @@
+// Here we have all the things that we have to import to our import.js
+
+
+module.exports = {
+    x: 10,
+    y: 20
+    // Note here we have used : colon because this is an object we can't use = here
+}
---
+++ unit4/import.js
@@ -0,0 +1,9 @@
+// How to import
+
+const a = require('./export')
+
+console.log(a)
+
+// if we require only one value then we have to write 
+console.log("Specific value from the exports.js is as follows")
+console.log(a.x);
