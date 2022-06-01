707c2f06a1b6310e276eb033229291a01d0b181e
abhinav3254 <abhinav3254@gmail.com>
2022-06-01 19:43:09 +0530

    unit5

---
+++ unit5/index.js
@@ -0,0 +1,7 @@
+// Consider modules to be the same as JavaScript libraries.
+// A set of functions you want to include in your application.
+
+console.log()
+// This is an example of core module because we havent't made it.
+// This is defined by default.
+
---
+++ unit5/readme.txt
@@ -0,0 +1,7 @@
+
+Core Module in Node js
+
+1. What are the core modules
+2. What are global modules
+3. Global Model example
+4. Non-global module with example
