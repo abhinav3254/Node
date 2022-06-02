df01b0eced91be6526235cf8974c51cc85e67be4
abhinav3254 <abhinav3254@gmail.com>
2022-06-02 15:23:12 +0530

    Modules

--- unit5/index.js
+++ unit5/index.js
@@ -5,3 +5,15 @@ console.log()
 // This is an example of core module because we havent't made it.
 // This is defined by default.
 
+
+
+// Global are those kinds of module whom we don't have to import.
+// example is console because we don't have to import console.
+
+const fs = require('fs');
+
+fs.writeFileSync("hello.txt", "Hello this is Abhinav Kumar learning Node js");
+
+// This has create a file named hello.txt in which we have written the text hello this.......
+// This is an example of non-global module
+
